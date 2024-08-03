597a7116b005a00ed3407cef75051fda858f39e2
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-08-04 00:11:45 +0300

    Dataların yüklenmesi belirli bir şarta bağlandı ve bu işlem ternary operatör kullanılarak gerçekleştirildi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,66 @@
-import { CORE_CONCEPTS } from './data';
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
-import TabButton from './components/TabButton/TabButton.jsx';
-import { useState } from 'react';
-import { EXAMPLES } from './data';
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
+import { useState } from "react";
+import { EXAMPLES } from "./data";
 
-function Main(){
-
-  const [selectedTopic, setSelectedTopic] = useState("components");
-  function handleSelect(selectedButton){
-     setSelectedTopic(selectedButton);
-    }
+function Main() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
 
   return (
     <div>
-      <main>     
-      <section id="core-concepts"> 
-        <h2> Core Concepts</h2>
-        <ul>
-          <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
-          <CoreConcept {...CORE_CONCEPTS[1]} /> {/* Daha kısa yol bu şekildedir böylece belirlenen indexdeki tüm propsları ilgili yerlerine aktarır */}
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-          <CoreConcept {...CORE_CONCEPTS[3]} />
-        </ul>
+      <main>
+        <section id="core-concepts">
+          <h2> Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />{" "}
+            {/* Daha kısa yol bu şekildedir böylece belirlenen indexdeki tüm propsları ilgili yerlerine aktarır */}
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
         <section id="examples">
           <h2> Examples </h2>
-          <menu> 
-            <TabButton onSelect={() => handleSelect('components')}> Components </TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}> JSX Button </TabButton>
-            <TabButton onSelect={() => handleSelect('props')}> Props Button </TabButton>
-            <TabButton onSelect={() => handleSelect('state')}> State Button </TabButton>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              {" "}
+              Components{" "}
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>
+              {" "}
+              JSX Button{" "}
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>
+              {" "}
+              Props Button{" "}
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>
+              {" "}
+              State Button{" "}
+            </TabButton>
           </menu>
-          <div id='tab-content'>
-            <h3> {EXAMPLES[selectedTopic].title} </h3>
-            <p> {EXAMPLES[selectedTopic].description}  </p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
 
-          </div>
+          {!selectedTopic ? (
+            <p> Please Select Topic </p>
+          ) : (
+            <div id="tab-content">
+              <h3> {EXAMPLES[selectedTopic].title} </h3>
+              <p> {EXAMPLES[selectedTopic].description} </p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
-
       </main>
     </div>
   );
@@ -54,7 +71,6 @@ function App() {
     <div>
       <Header />
       <Main />
-     
     </div>
   );
 }
